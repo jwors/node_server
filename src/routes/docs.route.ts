d883e58683a062d98671dcd5982c63ca819edc85
jwors <j.wors@outlook.com>
2024-10-18 00:17:50 +0800

    feat:提交

--- src/routes/docs.route.ts
+++ src/routes/docs.route.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/getDocs', (req, res) => {
-	res.send('ok!!!')
+	res.writeHead(200, {
+		"Content-Type": "text/plan;charset=utf-8"
+	})
+	const feadBackData = {
+		data: {
+			number: 1,
+			age: 18,
+			name: '曹浩洋'
+		},
+		status: 200,
+		code: 200
+	}
+	res.end(JSON.stringify(feadBackData))
 })
 
 export default router
